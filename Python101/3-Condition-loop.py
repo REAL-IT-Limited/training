787cf67e5fb6d3ae60bbb320dd0a44be82563d74
pairochjulrat <pairochjulrat@gmail.com>
2017-06-25 00:08:03 +0700

    Day 1

---
+++ Python101/3-Condition-loop.py
@@ -0,0 +1,28 @@
+#for i in range(start,stop, <incremental>):
+for i in range(1,10,2):
+    print(i)
+    print("in loop")
+print("I am out of loop")
+
+L1 = [10.1, 23, 45, 33]
+for i in L1:
+    print(i)
+
+i=0
+while i<10:
+    print(i)
+    i = i + 1
+
+i = 10
+k = 1
+if(i<k):
+    print("i less than k")
+
+elif(i>k):
+    print("i greater then k")
+else:
+    print("it's equal")
+
+
+
+
