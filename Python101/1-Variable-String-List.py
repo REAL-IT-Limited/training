70c2aae479a7b1176770de04640c43750269f597
pairochjulrat <pairochjulrat@gmail.com>
2017-06-25 00:07:20 +0700

    Day 1

---
+++ Python101/1-Variable-String-List.py
@@ -0,0 +1,50 @@
+
+s = "Hello"
+y = "Thailand"
+
+print(s + y)
+
+k = 10.6
+y = 5.1
+print (k + y)
+
+#placeholder
+
+"""
+multiple line using triple quotation mark
+ hdfjas
+ ksdfads
+ """
+
+msg = "Date/Time= %s, temp= %d "
+myDateTime = "xxxx"
+myTemp =66
+print(msg%(myDateTime,myTemp))
+
+L1 = [10.1, 23, 45, 33]
+print(len(L1))
+print(L1)
+print(L1[0])
+print(L1[2])
+print(L1[0]+L1[1])
+
+del L1[1]
+print(L1)
+
+#del L1
+
+print(len(L1))
+
+print(max(L1))
+print(min(L1))
+
+print(sum(L1))
+
+print(sum(L1)/len(L1))
+
+L1.append(77)
+L1.append(88)
+print(L1)
+
+L2 = [1 ,3 ,10]
+print(L1+L2)
